5e46ce26cfc74b7e6d3c7ccabfab5e664c91555f
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-12-01 10:18:19 -0500

    fix query param gen

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export const formatAPIObject = (endpoint: string, obj: Object) => {
   if(obj == null) return endpoint;
   if(endpoint.endsWith("/")) endpoint = endpoint.substring(0, endpoint.length - 1) + "?";
+  else endpoint = endpoint + "?";
   Object.entries(obj).forEach(([key, val]) => endpoint += `${key}=${val}&`);
   return endpoint.substring(0, endpoint.length - 1);
 }
@@ -8,6 +9,7 @@ export const formatAPIObject = (endpoint: string, obj: Object) => {
 export const formatAPIFormData = (endpoint: string, obj: FormData) => {
   if(obj == null) return endpoint;
   if(endpoint.endsWith("/")) endpoint = endpoint.substring(0, endpoint.length - 1) + "?";
+  else endpoint = endpoint + "?";
   obj.entries().forEach(([key,val]) => endpoint += `${key}=${val}&`);
   return endpoint.substring(0, endpoint.length - 1);
 }
