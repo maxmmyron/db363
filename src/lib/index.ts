bacf135d49f81deb1675091d9cadad61d63f2aeb
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-12-02 17:28:53 -0500

    remove redundant method

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,11 +5,3 @@ export const formatAPIObject = (endpoint: string, obj: Object) => {
   Object.entries(obj).forEach(([key, val]) => endpoint += `${key}=${val}&`);
   return endpoint.substring(0, endpoint.length - 1);
 }
-
-export const formatAPIFormData = (endpoint: string, obj: FormData) => {
-  if(obj == null) return endpoint;
-  if(endpoint.endsWith("/")) endpoint = endpoint.substring(0, endpoint.length - 1) + "?";
-  else endpoint = endpoint + "?";
-  obj.entries().forEach(([key,val]) => endpoint += `${key}=${val}&`);
-  return endpoint.substring(0, endpoint.length - 1);
-}
