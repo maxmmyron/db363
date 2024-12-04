b6c3922a55800c9e2398cacd46b527ac30a5b357
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-12-04 17:19:09 -0500

    Update train schema to include current link

--- src/main/java/com/mmyron/db363/entitiy/Train.java
+++ src/main/java/com/mmyron/db363/entitiy/Train.java
@@ -30,26 +30,33 @@ public class Train {
 	@ManyToOne
 	@JoinColumn(name="station_name", referencedColumnName = "name", nullable = true)
 	private Station station;
-	
+
+	@ManyToOne
+	@JoinColumns(
+		@JoinColumn(name="link_origin", referencedColumnName="origin_name", nullable=true)
+		@JoinColumn(name="link_dest", referencedColumnName="dest_name", nullable=true)
+	)
+	private Link link;
+
 	@Column(name="station_departure")
 	private Time stationDep = null;
-	
+
 	@Column(name="station_arrival")
 	private Time stationArrival = null;
-	
+
 	@ManyToOne
 	@JoinColumns({
 		@JoinColumn(name="schedule_id", referencedColumnName = "id", nullable = true),
 		@JoinColumn(name="schedule_route", referencedColumnName = "origin_route", nullable = true)
 	})
 	private Schedule schedule = null;
-	
+
 	@Column(name="schedule_departure")
 	private Time schedDep;
-	
+
 	@Column(name="train_status", nullable = false)
 	private String status;
-	
+
 	@OneToMany(mappedBy = "train")
 	private Set<Ticket> tickets = new HashSet<>();
 
@@ -59,7 +66,7 @@ public class Train {
 		this.station = station;
 		this.status = trainStatus;
 	}
-	
+
 	public Train(Station station, String trainStatus, Schedule schedule) {
 		this.station = station;
 		this.status = trainStatus;
@@ -99,7 +106,7 @@ public class Train {
 	public void setSchedule(Schedule schedule) {
 		this.schedule = schedule;
 	}
-	
+
 	// overrides
 
 	@Override
