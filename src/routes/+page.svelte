8e5bd38189070e025b2d381279b2d6ecea06c3bd
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-12-08 01:25:39 -0500

    better viz for trains

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,12 @@
   let d = $state(data);
   $effect(() => console.log(d));
 
-  let time = $state(new Date("2024-01-01T08:00:00").getTime());
+  let time = $state(new Date("2024-01-01T08:00:00+00:00").getTime());
   $effect(() => {
     tick(time).then(async () => {
       console.log("tick complete... updating trains");
       let res = await fetch("http://localhost:5133/api/trains/");
-      data.trains = await res.json();
+      d.trains = await res.json();
     });
   });
 
@@ -100,46 +100,55 @@
     ];
 
     let lines: {
-      [route: string]: Array<{
-        part: App.Station | App.Link;
-        trains: App.Train[];
-      }>;
+      [route: string]: {
+        color: string;
+        parts: Array<{
+          part: App.Station | App.Link;
+          trains: App.Train[];
+          isTransfer: boolean;
+        }>;
+      };
     } = {
-      "Line 1": [],
-      "Line 2": [],
-      "Line 3": [],
-      "Line 4": [],
+      "Line 1": { color: "#8A4F7D", parts: [] },
+      "Line 2": { color: "#F6CACA", parts: [] },
+      "Line 3": { color: "#B3DEC1", parts: [] },
+      "Line 4": { color: "#ECA400", parts: [] },
     };
 
     for (const [r, line] of Object.entries(lines)) {
       const { ordering } = orderings.find((o) => o.line === r)!;
       for (let i = 0; i < ordering.length; i++) {
-        const n = ordering[i];
-        let s = d.stations.find((s) => s.route === r && s.name === n)!;
-        let l = d.links.find(
-          (s) => s.origin.route === r && s.origin.name === n
+        let namedStations = d.stations.filter((s) => s.name === ordering[i]);
+        let station = namedStations.find((s) => s.route === r)!;
+        let link = d.links.find(
+          (s) => s.origin.route === r && s.origin.name === ordering[i]
         )!;
-        line.push({
-          part: s,
+
+        // add new station
+        line.parts.push({
+          part: station,
+          isTransfer: namedStations.length > 1,
           trains: d.trains.filter(
-            (t) => t.station?.name == s.name && t.station?.route === s.route
+            (t) =>
+              t.station?.name == station.name &&
+              t.station?.route === station.route
           ),
         });
 
+        // if we're not at last station, add new link
         if (i === ordering.length - 1) continue;
-        line.push({
-          part: l,
+        line.parts.push({
+          part: link,
+          isTransfer: false,
           trains: d.trains.filter(
             (t) =>
               t.station === null &&
-              t.link.origin.name === l.origin.name &&
-              t.link.origin.route == l.origin.route
+              t.link.origin.name === link.origin.name &&
+              t.link.origin.route == link.origin.route
           ),
         });
       }
     }
-    console.log(d.trains);
-    console.log(lines);
     return lines;
   });
 
@@ -155,7 +164,9 @@
 
 <section>
   {#key time}
-    <p class="">time: {new Date(time).toString()}</p>
+    <p class="">
+      time: {new Date(time).toLocaleString("en-US", { timeZone: "UTC" })}
+    </p>
   {/key}
   <button onclick={() => (paused = !paused)} class="border"
     >{paused ? "unpause" : "pause"}</button
@@ -163,35 +174,69 @@
   <button onclick={() => (time += 1000 * 60)} class="border">tick</button>
 
   {#if ziplines}
-    {#each Object.entries(ziplines) as [line, zip]}
-      <section>
-        <h1>{line}</h1>
-        <div class="flex">
-          {#each zip as leg, i}
-            {#if i % 2 === 0}
-              <div class="flex flex-col gap-1 items-center justify-center">
-                {#each leg.trains as train}
-                  <div class="flex gap-0.5 w-full justify-center">
-                    <div class="w-2 h-4 rounded-md bg-blue">{train.id}</div>
+    {#each Object.entries(ziplines) as [line, { parts, color }]}
+      <div class="grid grid-rows-3 h-9 grid-flow-col mb-24 w-full">
+        <div
+          class="col-start-1 row-start-2 rounded-xl w-full z-0"
+          style="background-color: {color}; grid-column-end: {Math.ceil(
+            parts.length / 2
+          ) + 1}"
+        ></div>
+        {#each parts as { part, isTransfer, trains }, i}
+          {#if i % 2 === 0}
+            <div
+              class="row-span-full grid grid-rows-subgrid z-10 place-items-center {i ==
+              parts.length - 1
+                ? 'grid-cols-[12px]'
+                : 'grid-cols-[12px,1fr]'}"
+              style="grid-column-start: {i / 2 + 1};"
+            >
+              <!-- Station: Trains -->
+              <div class="row-start-1">
+                {#each trains as train}
+                  <div class="w-2 h-3 rounded-full bg-green-500">
+                    {train.id}
                   </div>
                 {/each}
-                <div class="rounded-full w-4 h-4 bg-black"></div>
-                <p>{(leg.part as App.Station).name}</p>
               </div>
-            {:else}
-              <div class="flex flex-col gap-1 items-center justify-center">
-                {#each leg.trains as train}
-                  <div class="flex gap-0.5 w-full justify-center">
-                    <div class="w-2 h-4 rounded-md bg-blue">{train.id}</div>
-                  </div>
-                {/each}
-                <div class="w-12 h-0.5 bg-neutral-600"></div>
-                <p></p>
+              <!-- Station styling -->
+              {#if isTransfer}
+                <div
+                  class="w-4 h-4 rounded-full border-2 border-neutral-800 bg-white {i ==
+                  parts.length - 1
+                    ? 'justify-end'
+                    : ''}"
+                ></div>
+              {:else}
+                <div
+                  class="w-2 h-2 rounded-full bg-white {i == parts.length - 1
+                    ? 'justify-end'
+                    : ''}"
+                ></div>
+              {/if}
+              <div class="relative">
+                <p
+                  class="absolute rotate-[50deg] origin-top-left top-1 -left-5 text-nowrap text-neutral-800"
+                >
+                  {(part as App.Station).name}
+                </p>
               </div>
-            {/if}
-          {/each}
-        </div>
-      </section>
+
+              <!-- show link -->
+              {#if i !== parts.length - 1}
+                {@const trains = parts[i + 1].trains}
+                <div class="row-start-1 col-start-2">
+                  {#each trains as train}
+                    <div class="w-2 h-3 rounded-full bg-green-500">
+                      {train.id}
+                    </div>
+                  {/each}
+                </div>
+              {/if}
+            </div>
+          {/if}
+        {/each}
+      </div>
     {/each}
   {/if}
 
